e3aa119d6b2d65d7def45d2068db62a13d921bc6
ThallysonKodex <thallysontrader@gmail.com>
2022-10-13 04:36:03 -0400

    Added sprite animation

--- entities/Player.py
+++ entities/Player.py
@@ -10,10 +10,16 @@ class Player(pygame.sprite.Sprite):
         super().__init__(group)
         self.group = group
         self.zed = 0
+        self.size = size
 
-        self.image = pygame.transform.scale(pygame.image.load("graphics/player/body/down/normal/0.png").convert_alpha(), (size, size * 1.2))
-        self.light = pygame.transform.scale(pygame.image.load("graphics/player/body/down/light/0.png").convert_alpha(), (size, size * 1.2))
-        self.shadow = pygame.transform.scale(pygame.image.load("graphics/player/body/down/shadow/0.png").convert_alpha(), (size, size * 1.2))
+        self.frame = 0
+        self.sprite_direction = 'down'
+        self.frames(self.sprite_direction)
+
+        self.image = self.frames_normal[self.frame]
+        self.light = self.frames_light[self.frame]
+        self.shadow = self.frames_shadow[self.frame]
+        self.custom = []
 
         self.rect = self.image.get_rect(center = pos)
 
@@ -24,6 +30,27 @@ class Player(pygame.sprite.Sprite):
         self.last_position_x = 'right'
         self.last_position_y = 'down'
 
+    def frames(self, sprite_direction):
+        self.frames_normal = [pygame.transform.scale(pygame.image.load(f"graphics/player/body/{sprite_direction}/normal/{frame}.png").convert_alpha(), (self.size, self.size * 1.2))
+                              for frame in range(7)]
+        self.frames_shadow = [pygame.transform.scale(pygame.image.load(f"graphics/player/body/{sprite_direction}/shadow/{frame}.png").convert_alpha(), (self.size, self.size * 1.2))
+                              for frame in range(7)]
+        self.frames_light = [pygame.transform.scale(pygame.image.load(f"graphics/player/body/{sprite_direction}/light/{frame}.png").convert_alpha(), (self.size, self.size * 1.2))
+                             for frame in range(7)]
+
+    def animation(self, dt):
+        if self.direction.magnitude() != 0:
+            self.frame += 1 * (self.speed / 10) * dt
+            if self.frame >= 7:
+                self.frame = 0
+            self.image = pygame.transform.scale(self.frames_normal[int(self.frame)], (self.size, self.size* 1.2))
+            self.shadow = pygame.transform.scale(self.frames_shadow[int(self.frame)], (self.size, self.size* 1.2))
+            self.light = pygame.transform.scale(self.frames_light[int(self.frame)], (self.size, self.size* 1.2))
+        else:
+            self.frame = 0
+        self.image = pygame.transform.scale(self.frames_normal[int(self.frame)], (self.size, self.size* 1.2))
+        self.shadow = pygame.transform.scale(self.frames_shadow[int(self.frame)], (self.size, self.size* 1.2))
+        self.light = pygame.transform.scale(self.frames_light[int(self.frame)], (self.size, self.size* 1.2))
 
     def movement(self, dt):
         if self.direction.magnitude() != 0:
@@ -35,18 +62,23 @@ class Player(pygame.sprite.Sprite):
         kb = pygame.key.get_pressed()
         if kb[pygame.K_w]:
             if self.direction.magnitude() == 0:
+                self.sprite_direction = 'up'
                 self.last_position_y = 'up'
                 self.direction.y = -1
+
         elif kb[pygame.K_s]:
             if self.direction.magnitude() == 0:
+                self.sprite_direction = 'down'
                 self.last_position_y = 'down'
                 self.direction.y = 1
         elif kb[pygame.K_d]:
             if self.direction.magnitude() == 0:
+                self.sprite_direction = 'right'
                 self.last_position_x = 'right'
                 self.direction.x = 1
         elif kb[pygame.K_a]:
             if self.direction.magnitude() == 0:
+                self.sprite_direction = 'left'
                 self.last_position_x = 'left'
                 self.direction.x = -1
 
@@ -82,10 +114,15 @@ class Player(pygame.sprite.Sprite):
                 self.direction = pygame.math.Vector2((0, 0))
 
     def update(self, dt):
-        print(int(self.position.y))
+        self.movement(dt)
+
+        self.frames(self.sprite_direction)
+        self.animation(dt)
+
         self.multiple_checker()
         self.input()
-        self.movement(dt)
+
+
 
 
 
